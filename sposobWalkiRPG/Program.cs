930d90d642fb8dc224a6ac9113fd334434c7bda8
Bartłomiej Kłuś <bartlomiej.klus@microsoft.wsei.edu.pl>
2022-11-02 20:16:45 +0100

    Mini gra w której gracz bije potwora

--- sposobWalkiRPG/Program.cs
+++ sposobWalkiRPG/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            
+
+            int playerLife = 10;
+            int monsterLife = 10;
+
+            do
+            {
+               int roll = random.Next(1, 11);
+               monsterLife -= roll;
+               Console.WriteLine($"Monster was damaged and lost {roll} and now has {monsterLife} healt");
+
+               if (monsterLife <= 0) continue;
+
+               roll = random.Next(1, 11);
+               playerLife -= roll;
+               Console.WriteLine($"Player was damaged and lost {roll} and now has {playerLife} healt");
+
+            } while(monsterLife > 0 && playerLife >0);
+
+            Console.WriteLine(playerLife>monsterLife ? "PLAYER WIN!": "MONSTER WIN!" );
         }
     }
 }
